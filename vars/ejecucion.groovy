76cd53d9a0c9c8a4a47a8ccb5c2d2a4f859b7c65
juansalinasaedo <juan.salinas.aedo@gmail.com>
2021-01-05 13:49:59 -0300

    actualizacion 05-01-21

--- vars/ejecucion.groovy
+++ vars/ejecucion.groovy
@@ -3,17 +3,22 @@ def call(){
   pipeline {
     agent any
 
-    parameters { 
+    /*parameters { 
         choice(name: 'herramientas', choices: ['gradle', 'maven'], description: 'Elección de herramienta de construcción para aplicación covid') 
-       // string(name: 'stage', defaultValue: '', description: '')
-    }
-
+        string(name: 'stage', defaultValue: '', description: '')
+    } */
+ 
     stages {
         stage('Pipeline') {
+            environment {
+              LAST_STAGE_NAME = ''
+            }
+
             steps {
                 script {
                  // env.TAREA = ''
                   println 'Herramientas de Ejecucion: ' + params.herramientas
+                  println 'Stage: ' + params.stage
 
                   if (params.buildtool == 'gradle'){
                     gradle.call()
@@ -26,18 +31,17 @@ def call(){
         }
     }
 
-
     post {
-        success {
-            slackSend color: 'good', message: "[Juan Salinas][pipeline-devops][${params.herramientas}] Ejecución exitosa."
-        }
-        failure {
-            slackSend color: 'danger', message: "[Juan Salinas][pipeline-devops][${params.herramientas}] Ejecución fallida en stage ${STAGE_NAME}."
+            success {
+                slackSend color: 'good', message: "[Juan Salinas][${env.JOB_NAME}][${params.buildtool}] Ejecución exitosa"
             }
-        }
-    }
 
-    
+            failure {
+                slackSend color: 'danger', message: "[Juan Salinas][${env.JOB_NAME}][${params.buildtool}] Ejecución fallida en stage ${env.LAST_STAGE_NAME}"
+            }
+    }    
+
+    }    
   }
 }
 
--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -1,48 +1,62 @@
 def call(){
+	stages = ['Build & Test', 'Sonar', 'Run', 'Rest', 'Nexus']
 
-	stage('Build & Test'){
-		//STAGE_NAME = 'Build & test'
-		env.TAREA = env.STAGE_NAME
-		sh "./gradlew clean build"
-	}	
+	// Si stage es vacio se toman todos los stages
+    _stage = params.stage ? params.stage.split(';') : stages
 
-	stage('Sonar'){
-		//STAGE_NAME = 'sonar'
-		env.TAREA = env.STAGE_NAME
-		//Generar instancia de tipo tool del scanner
-		//Va el nombre de la instancia en Jenkins>Global tool config.
-		//def scannerHome = tool 'sonar-scanner';
-		//Corresponde a lo  configurado en Jenkins>Configurar el sistema
-		/*withSonarQubeEnv('sonar') {
-			sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build"
-		}*/
-		stage('SonarQube Analisis') {
-			env.TAREA = env.STAGE_NAME
+    // Se valida el stage ingresado
+    _stage.each { el ->
+        if (!stages.contains(el)) {
+            throw new Exception("Stage: $el no es una opción válida.")
+        }
+    }
+
+    if(_stage.contains('Build & Test')) {
+		stage('Build & Test'){
+			//STAGE_NAME = 'Build & test'
+			//env.TAREA = env.STAGE_NAME
+			sh "./gradlew clean build"
+		}	
+	}
+
+	if(_stage.contains('Sonar')) {
+		stage('Sonar'){
+			env.LAST_STAGE_NAME = env.STAGE_NAME
 			def scannerHome = tool 'sonar-scanner';
-			withSonarQubeEnv('Sonar-Server') {
-				sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build"
-			}
+
+				withSonarQubeEnv('Sonar-Server') {
+					sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build"
+				}
+			
 		}
 	}
 
-	stage('Run'){
-		//STAGE_NAME = 'Run'
-		env.TAREA = env.STAGE_NAME
-		//sh "nohup bash gradlew bootRun &"
-		sh "nohup gradle bootRun &"
-		sleep 30
+
+	if(_stage.contains('Run')) {
+		stage('Run'){
+			//STAGE_NAME = 'Run'
+			env.LAST_STAGE_NAME = env.STAGE_NAME
+			//sh "nohup bash gradlew bootRun &"
+			sh "nohup gradle bootRun &"
+			sleep 30
+		}
 	}
 
-	stage('Rest'){
-		//STAGE_NAME = 'Test'
-		env.TAREA = env.STAGE_NAME
-		sh "curl -X GET 'http://localhost:8082/rest/mscovid/test?msg=testing'"
+
+	if(_stage.contains('Rest')) {
+		stage('Rest'){
+			//STAGE_NAME = 'Test'
+			env.LAST_STAGE_NAME = env.STAGE_NAME
+			sh "curl -X GET 'http://localhost:8082/rest/mscovid/test?msg=testing'"
+		}
 	}
 
-	stage('Nexus'){
-		//STAGE_NAME = 'Nexus'
-		env.TAREA = env.STAGE_NAME
-		nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: '/home/juan/ejemplo-maven-1/build/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
+	if(_stage.contains('Nexus')) {
+		stage('Nexus'){
+			//STAGE_NAME = 'Nexus'
+			env.LAST_STAGE_NAME = env.STAGE_NAME
+			nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: '/home/juan/ejemplo-maven-1/build/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
+		}
 	}
 }
 
