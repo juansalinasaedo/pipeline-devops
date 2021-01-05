22c66db549b2a9ec1ce1012c0f59980ccecdecf4
juansalinasaedo <juan.salinas.aedo@gmail.com>
2021-01-05 14:03:44 -0300

    actualizacion 05-01-21 para rama feature-inicial

--- vars/ejecucion.groovy
+++ vars/ejecucion.groovy
@@ -1,40 +1,48 @@
 def call(){
   
-	pipeline {
+  pipeline {
     agent any
-	
-	parameters { choice(name: 'devtool', choices: ['maven', 'gradle'], description: 'Elección de herramienta de construcción para aplicación covid') }
 
+    /*parameters { 
+        choice(name: 'herramientas', choices: ['gradle', 'maven'], description: 'Elección de herramienta de construcción para aplicación covid') 
+        string(name: 'stage', defaultValue: '', description: '')
+    } */
+ 
     stages {
         stage('Pipeline') {
+            environment {
+              LAST_STAGE_NAME = ''
+            }
+
             steps {
                 script {
-					def STAGE_NAME = ''
-					params.devtool
-				
-					/*def pipe = (params.devtool == 'gradle') ? load("gradle.groovy") : load("maven.groovy")
-					pipe.call()*/
+                 // env.TAREA = ''
+                  println 'Herramientas de Ejecucion: ' + params.herramientas
+                  println 'Stage: ' + params.stage
 
-					if (params.buildtool == 'gradle'){
-					    gradle.call()	
-					} else {
-					    maven.call()
-					}
-				}
-           }
+                  if (params.buildtool == 'gradle'){
+                    gradle.call()
+                  } else {
+                    maven.call()
+                  }
+
+                }
+            }
         }
     }
-	
-	post {
-		success {
-			slackSend color: 'good', message: "[Juan Salinas][pipeline-maven-gradle][${params.devtool}] Ejecución exitosa."
-		}
-		failure {
-			slackSend color: 'danger', message: "[Juan Salinas][pipeline-maven-gradle][${params.devtool}] Ejecución fallida en stage ${STAGE_NAME}."
-			}
-		}
-	}
 
+    post {
+            success {
+                slackSend color: 'good', message: "[Juan Salinas][${env.JOB_NAME}][${params.buildtool}] Ejecución exitosa"
+            }
+
+            failure {
+                slackSend color: 'danger', message: "[Juan Salinas][${env.JOB_NAME}][${params.buildtool}] Ejecución fallida en stage ${env.LAST_STAGE_NAME}"
+            }
+    }    
+
+    }    
+  }
 }
 
 return this;
