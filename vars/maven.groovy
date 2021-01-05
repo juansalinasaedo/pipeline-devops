e0e368cdfd114ef8716e49fca4a976d4e0c65e14
juansalinasaedo <juan.salinas.aedo@gmail.com>
2021-01-05 14:05:28 -0300

    actualizacion 05-01-21 para rama feature-inicial

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -1,36 +1,58 @@
 def call() {
-  
-    stage('Compile'){
-		STAGE_NAME = 'Compile'
-		sh 'mvn clean compile -e'
-	}
 
-    stage('Test'){
-		STAGE_NAME  = 'Test'
-		sh 'mvn clean test -e'
-	}
+	stages = ['Compile', 'Test', 'Jar', 'SonarQube', 'uploadNexus']
+
+	// Si stage es vacio se toman todos los stages
+    _stage = params.stage ? params.stage.split(';') : stages
+
+    // Se valida el stage ingresado
+    _stage.each { el ->
+        if (!stages.contains(el)) {
+            throw new Exception("Stage: $el no es una opción válida.")
+        }
+    }
+  
+  	if(_stage.contains('Compile')) {
+	    stage('Compile'){
+	    	 env.LAST_STAGE_NAME = env.STAGE_NAME
+	    	 sh 'mvn clean compile -e'
+		}
+	}	
+
+    if(_stage.contains('Test')) {
+	    stage('Test'){
+	    	env.LAST_STAGE_NAME = env.STAGE_NAME
+			sh 'mvn clean test -e'
+		}
+	}	
 
       
-    stage('Jar'){
-		STAGE_NAME = Jar
-		sh 'mvn clean package -e'
-		sleep 30
-	}
-
-
-      stage('SonarQube'){
-		STAGE_NAME = 'SonarQube'
-		withSonarQubeEnv(installationName: 'sonar'){
-			sh 'mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.7.0.1746:sonar'
+    if(_stage.contains('Jar')) {  
+	    stage('Jar'){
+			env.LAST_STAGE_NAME = env.STAGE_NAME
+			sh 'mvn clean package -e'
+			sleep 30
 		}
 	}
 
 
+    if(_stage.contains('SonarQube')) {
+	     stage('SonarQube'){
+			env.LAST_STAGE_NAME = env.STAGE_NAME
+			withSonarQubeEnv(installationName: 'sonar'){
+				sh 'mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.7.0.1746:sonar'
+			}
+		}
+	}
+
+
+	if(_stage.contains('uploadNexus')) {
       stage('uploadNexus') {
-          STAGE_NAME = 'uploadNexus'
+          env.LAST_STAGE_NAME = env.STAGE_NAME
           nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'build/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
          
-       }   
+       }  
+    }    
 }
 
 return this;
