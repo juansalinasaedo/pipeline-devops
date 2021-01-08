fd0ff68c2e512f425d61d92d712640b88f29ff80
juansalinasaedo <juan.salinas.aedo@gmail.com>
2021-01-08 00:41:24 -0300

    actualizacion taller 15

--- vars/gradle_cd.groovy
+++ vars/gradle_cd.groovy
@@ -1,18 +1,18 @@
 def call(){
   
-        def downloadOK = false;
+        def descarga = false;
         figlet 'Gradle CD'
 
         stage("downloadNexus"){    
             env.TAREA =  env.STAGE_NAME       
             sh 'curl -X GET -u admin:admin http://localhost:8081/repository/test-nexus/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar -O' 
-            downloadOK = true;
+            descarga = true;
         }
 
 
         stage("runDownloadedJar"){    
             env.TAREA =  env.STAGE_NAME   
-            if (downloadOK) {
+            if (descarga) {
                 sh "java -jar DevOpsUsach2020-0.0.1.jar &"
                 sleep 20   
             }             
@@ -20,13 +20,13 @@ def call(){
 
         stage("rest"){
             env.TAREA =  env.STAGE_NAME 
-            if (downloadOK) 
-                sh 'curl -X GET "http://localhost:8082/rest/mscovid/test?msg=testing"'
+            if (descarga) 
+                //sh 'curl -X GET "http://localhost:8082/rest/mscovid/test?msg=testing"'
         }  
 
         stage("nexusCD"){    
             env.TAREA =  env.STAGE_NAME   
-            if (downloadOK)          
+            if (descarga)          
                 nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'build/libs/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: 'release-v1.0.0']]]                     
         }                    
 
