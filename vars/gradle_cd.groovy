39147f3bab09c54c19877634059bb54394dbd802
juansalinasaedo <juan.salinas.aedo@gmail.com>
2021-01-08 00:01:13 -0300

    actualizacion taller 15

--- vars/gradle_cd.groovy
+++ vars/gradle_cd.groovy
@@ -28,7 +28,7 @@ def call(){
         stage("nexusCD"){    
             env.TAREA =  env.STAGE_NAME   
             if (downloadOK)          
-                nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: 'release-v1.0.0']]]                     
+                nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'build/libs/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: 'release-v1.0.0']]]                     
         }                    
 
 }
